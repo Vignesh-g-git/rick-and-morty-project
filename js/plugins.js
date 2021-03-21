cccb9352b7ad78f9fefe766aa7dba2a129f2dee8
swastik <swastik7999@gmail.com>
2021-03-21 09:12:31 +0530

    starter-8 commit

--- js/plugins.js
+++ js/plugins.js
@@ -1,15 +1,35 @@
 // Avoid `console` errors in browsers that lack a console.
-(function() {
-  var method;
-  var noop = function () {};
-  var methods = [
-    'assert', 'clear', 'count', 'debug', 'dir', 'dirxml', 'error',
-    'exception', 'group', 'groupCollapsed', 'groupEnd', 'info', 'log',
-    'markTimeline', 'profile', 'profileEnd', 'table', 'time', 'timeEnd',
-    'timeline', 'timelineEnd', 'timeStamp', 'trace', 'warn'
+(function () {
+  let method;
+  const noop = function () {};
+  const methods = [
+    'assert',
+    'clear',
+    'count',
+    'debug',
+    'dir',
+    'dirxml',
+    'error',
+    'exception',
+    'group',
+    'groupCollapsed',
+    'groupEnd',
+    'info',
+    'log',
+    'markTimeline',
+    'profile',
+    'profileEnd',
+    'table',
+    'time',
+    'timeEnd',
+    'timeline',
+    'timelineEnd',
+    'timeStamp',
+    'trace',
+    'warn',
   ];
-  var length = methods.length;
-  var console = (window.console = window.console || {});
+  let { length } = methods;
+  const console = (window.console = window.console || {});
 
   while (length--) {
     method = methods[length];
@@ -19,6 +39,6 @@
       console[method] = noop;
     }
   }
-}());
+})();
 
 // Place any jQuery/helper plugins in here.
